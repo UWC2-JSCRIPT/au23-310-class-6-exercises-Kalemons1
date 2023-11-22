541fa9d3e0fb8e84fb6bff8be49b098aadeedefc
Kyle Lemons <59343513+Kalemons1@users.noreply.github.com>
2023-11-21 17:42:36 -0800

    Add files via upload

--- carProto.js
+++ carProto.js
@@ -5,6 +5,33 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class Car {
+    constructor(model) {
+        this.model = model;
+        this.currentSpeed = 0;
+        this.accelerate = () => {
+            this.currentSpeed++;
+        }
+        this.brake = () => {
+            this.currentSpeed--;
+        }
+    }
+    accelerate = () => {
+        this.currentSpeed++;
+    }
+    brake = () => {
+        this.currentSpeed--;
+    }
+    toString() {
+        return `Model: ${this.model} going at ${this.currentSpeed} mile per hour!`;
+    }
+}
+
+const porsche = new Car('porsche');
+porsche.accelerate();
+porsche.accelerate();
+porsche.brake();
+console.log(porsche.toString());
 
 /**
  * ElectricCar class
@@ -13,3 +40,32 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+//Accelerates twice as fast because electric 
+class ElectricCar {
+    constructor(model) {
+        this.model = model;
+        this.currentSpeed = 0;
+        this.accelerate = () => {
+            this.currentSpeed += 2;
+        }
+        this.brake = () => {
+            this.currentSpeed--;
+        }
+    }
+    accelerate = () => {
+        this.currentSpeed++;
+    }
+    brake = () => {
+        this.currentSpeed--;
+    }
+    toString() {
+        return `Model: ${this.model} going at ${this.currentSpeed} mile per hour!`;
+    }
+}
+
+const rivian = new ElectricCar('Rivian');
+rivian.accelerate();
+rivian.accelerate();
+rivian.brake();
+console.log(rivian.toString());
+
--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,24 @@
 // Do not change
+$(document).ready(function() {
 document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
 });
 
+
+
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+$('#more-info').click(function(event) {
+  event.preventDefault(); // Prevent the default
+  alert("Here's some info");
+});
+
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+$('#dog').click(function(event) {
+  event.stopPropagation(); // Stop the event
+  alert('Bow wow!');
+});
+
+});
